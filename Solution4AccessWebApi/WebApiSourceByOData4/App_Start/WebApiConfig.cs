e579cb263f94b66bd620ad16e1646ae4725459c7
HUDWARE <udoughong@hotmail.com>
2018-10-13 03:37:41 +0800

    201810130337

---
+++ Solution4AccessWebApi/WebApiSourceByOData4/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using System.Web.Http;
+using WebApiSourceByOData4.Models;
+
+namespace WebApiSourceByOData4
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API 設定和服務
+
+            // Web API 路由
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // New code:
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Product>("Products");
+            config.MapODataServiceRoute(
+                routeName: "ODataRoute",
+                routePrefix: null,
+                model: builder.GetEdmModel());
+        }
+    }
+}
---
+++ Solution4AccessWebApi/WebApiSourceByOData4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApiSourceByOData4.WebApiApplication" Language="C#" %>
